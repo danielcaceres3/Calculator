c8108570d075ff3d1ef2b79aa797db8f76ac739f
Daniel <dcaceres70@gmail.com>
2022-09-14 14:53:32 -0500

    Calculator is working. Testing for bugss

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 function add(a, b){
+    a = parseFloat(a);
+    b = parseFloat(b);
     return a + b; // return tha sum of the wo inputs
 };
 
 function subtract(a, b){
+    a = parseFloat(a);
+    b = parseFloat(b);
     return a - b; // subtract second number from the first number 
 };
 
 function multiply(a, b){
+    a = parseFloat(a);
+    b = parseFloat(b);
     return a * b; //Multiply both numbers by eachother 
 };
 
 function divide(a, b){
+    a = parseFloat(a);
+    b = parseFloat(b);
     return a/b; // Divide the second number by the first number
 };
 
@@ -28,14 +36,75 @@ function operate(op, a, b){
     }
 }
 
-const btns = document.querySelectorAll("button");
-let answer = document.getElementsByClassName('answer');
-console.log(btns);
 
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operators");
+const equals = document.querySelector(".equals");
+let answer = document.getElementById('answer');
+ 
 
-btns.forEach(function(btn){
-    btn.addEventListener("click", () => {
-        answer.textContent = btn.textContent
-        console.log(answer.textContent)
+let firstNum = "";
+let secondNum = "";
+let operation = "";
+let result = "";
+
+//gets the numbers and add it
+numbers.forEach(function(number){
+    number.addEventListener("click", number => {
+        if (operation === ""){
+            firstNum += number.target.innerText;
+            answer.textContent = firstNum;
+            console.log(firstNum);
+        }
+        else{
+            secondNum += number.target.innerText;
+            answer.textContent = secondNum
+            console.log(secondNum);
+        }
     });
 });
+
+//check the operation that is needed
+operators.forEach(function(operand){
+    operand.addEventListener("click", () => { 
+        operation = operand.innerText;
+        console.log(operation);
+    })
+})
+
+//check to see if equals is pressed
+equals.addEventListener("click", () =>{
+    result = operate(operation, firstNum, secondNum);
+    answer.textContent = result;
+    if(result == null){
+        answer.textContent = "Undefined"
+    }
+    firstNum = result;
+    secondNum = "";
+    console.log(result);
+}
+
+)
+
+//backspace button This will elimante the last number in the string
+const del = document.querySelector(".del");
+del.addEventListener("click", () =>{
+    answer.textContent = answer.textContent.slice(0, -1);
+    if(operation === ""){
+        firstNum = firstNum.slice(0,-1);
+    }
+    else{
+        secondNum = secondNum.slice(0,-1);
+    }
+
+})
+
+//this is the clear value working!
+const clear = document.querySelector(".clear")
+clear.addEventListener("click", () =>{
+    firstNum = "";
+    secondNum = "";
+    operation = "";
+    answer.textContent = "";
+})
+
